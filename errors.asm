0139b94042ffe21708d1be2a6ac761eab6644021
JoJo-1973 <stefano.priore@gmail.com>
2025-12-25 22:45:25 +0100

    Primo invio

---
+++ errors.asm
@@ -0,0 +1,34 @@
+; Messaggi d'errore del BASIC 2.0
+
+ERR_TOO_MANY_FILES        = $01
+ERR_FILE_OPEN             = $02
+ERR_FILE_NOT_OPEN         = $03
+ERR_FILE_NOT_FOUND        = $04
+ERR_DEVICE_NOT_PRESENT    = $05
+ERR_NOT_INPUT_FILE        = $06
+ERR_NOT_OUTPUT_FILE       = $07
+ERR_MISSING_FILENAME      = $08
+ERR_ILLEGAL_DEVICE_NUMBER = $09
+ERR_NEXT_WITHOUT_FOR      = $0A
+ERR_SYNTAX                = $0B
+ERR_RETURN_WITHOUT_GOSUB  = $0C
+ERR_OUT_OF_DATA           = $0D
+ERR_ILLEGAL_QUANTITY      = $0E
+ERR_OVERFLOW              = $0F
+ERR_OUT_OF_MEMORY         = $10
+ERR_UNDEFD_STATEMENT      = $11
+ERR_BAD_SUBSCRIPT         = $12
+ERR_REDIMD_ARRAY          = $13
+ERR_DIVISION_BY_ZERO      = $14
+ERR_ILLEGAL_DIRECT        = $15
+ERR_TYPE_MISMATCH         = $16
+ERR_STRING_TOO_LONG       = $17
+ERR_FILE_DATA             = $18
+ERR_FORMULA_TOO_COMPLEX   = $19
+ERR_CANT_CONTINUE         = $1A
+ERR_UNDEFD_FUNCTION       = $1B
+ERR_VERIFY                = $1C
+ERR_LOAD                  = $1D
+ERR_BREAK                 = $1E
+
+ERR_READY                 = $80
